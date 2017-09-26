545b978b91d351a2a06c0e1edfb75182dc5f02a3
Paul Craig <paul_craig_16@hotmail.com>
2017-09-26 03:41:29 +0100

    Add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Bootstrap virtualenv environment and requirements locally
+#
+# NOTE: This script expects to be run from the project root with
+# ./bootstrap.sh
+
+set -o pipefail
+
+if [ ! $VIRTUAL_ENV ]; then
+  virtualenv -p python3 ./venv
+fi
+
+venv/bin/pip install "pip>=8.0"
+
+venv/bin/pip install -r requirements.txt
+
+. ./venv/bin/activate
